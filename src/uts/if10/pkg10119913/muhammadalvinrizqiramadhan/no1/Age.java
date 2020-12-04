2d53adc5283a9ef7cd9ff32db388edc49601fb48
Dilan <ramadhanrizqi21@gmail.com>
2020-12-04 21:58:30 +0700

    Add Name and several code

--- src/uts/if10/pkg10119913/muhammadalvinrizqiramadhan/no1/Age.java
+++ src/uts/if10/pkg10119913/muhammadalvinrizqiramadhan/no1/Age.java
@@ -10,30 +10,55 @@ package uts.if10.pkg10119913.muhammadalvinrizqiramadhan.no1;
  * @author Lenovo
  */
 public class Age {
-    int yeartBirth;
-    int yearNow;
-    String red;
+    private int yearBirth;
+    private int yearNow;
+    private String red = "\33[31m";
 
-    public Age(int yeartBirth) {
-        this.yeartBirth = yeartBirth;
+    public Age(int yearNow) {
+        this.yearNow = yearNow;
+    }
+    
+
+
+    public int getYearBirth(int nextInt) {
+        return yearBirth;
     }
 
-    public int getYeartBirth() {
-        return yeartBirth;
+    public void setYearBirth(int yearBirth) {
+        this.yearBirth = yearBirth;
     }
 
     public int getYearNow() {
+
         return yearNow;
     }
-
-    public void setYeartBirth(int yeartBirth) {
-        this.yeartBirth = yeartBirth;
-    }
+    
     public int hitungUmur(){
-        return 0;
+        return yearNow-yearBirth;
     }
-    public String tandanyaKamu(int umur){
-        return "";
+    
+    public String tandanyaKamu (int umur){
+        String tandanya = null;
+        
+        if ((0<=umur)&&(umur<=5)){
+                tandanya = (red+"LAGI LUCU-LUCUNYA");
+        }else if ((5<umur)&&(umur<=10)){
+                tandanya = (red+"MASIH ANAK-ANAK");
+        }else if ((10<umur)&&(umur<=13)){
+                tandanya = (red+"MASIH REMADJA");
+        }else if ((13<umur)&&(umur<=19)){
+                tandanya = (red+"ALAY");
+        }else if ((19<umur)&&(umur<=29)){
+                tandanya = (red+"LAGI GALAU NYARI JODOH");
+        }else if ((29<umur)&&(umur<=35)){
+                tandanya = (red+"LAGI SIBUK NYARI UANG");
+        }else if ((35<umur)&&(umur<=150)){
+                tandanya = (red+"SUDAH TUA");
+        }else {
+                tandanya = (red+"TIDAK TERDETEKSI DI KEHIDUPAN");
+        }
+        return tandanya;
+        
     }
     
 }
--- src/uts/if10/pkg10119913/muhammadalvinrizqiramadhan/no1/UTSIF1010119913MuhammadAlvinRizqiRamadhanNo1.java
+++ src/uts/if10/pkg10119913/muhammadalvinrizqiramadhan/no1/UTSIF1010119913MuhammadAlvinRizqiRamadhanNo1.java
@@ -5,9 +5,16 @@
  */
 package uts.if10.pkg10119913.muhammadalvinrizqiramadhan.no1;
 
+import java.time.Year;
+import java.util.Scanner;
+
 /**
  *
- * @author Lenovo
+ * @author 
+ * Nama : Muhammad Alvin Rizqi Ramadhan
+ * Kelas : IF 10K
+ * NIM : 10119913
+ * Deskripsi : UTS no 1
  */
 public class UTSIF1010119913MuhammadAlvinRizqiRamadhanNo1 {
 
@@ -15,7 +22,22 @@ public class UTSIF1010119913MuhammadAlvinRizqiRamadhanNo1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+Scanner scanner = new Scanner(System.in);
+        int tahunLahir, tahunSekarang;
+
+        tahunSekarang = Year.now().getValue();
+        Age age = new Age(tahunSekarang);
+
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Masukkan Tahun lahir anda : ");
+        age.setYearBirth(scn.nextInt());
+        
+        System.out.println("======Hasil Perhitungan Umur======");
+        System.out.println("Tahun Lahir Anda : " + age.getYearBirth(0));
+        System.out.println("Hari ini Tahun : " + age.getYearNow());
+        System.out.println("Umur kamu sampai hari ini adalah "
+                + age.hitungUmur()+ " tahun");
+        System.out.println("Tandanya Kamu : "+ age.tandanyaKamu(age.hitungUmur()));
     }
     
 }
