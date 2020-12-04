00cd961e9c3896c574c6c770ea8845611fb4413d
Dilan <ramadhanrizqi21@gmail.com>
2020-12-04 20:32:07 +0700

    First Commit

---
+++ src/uts/if10/pkg10119913/muhammadalvinrizqiramadhan/no1/Age.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if10.pkg10119913.muhammadalvinrizqiramadhan.no1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Age {
+    int yeartBirth;
+    int yearNow;
+    String red;
+
+    public Age(int yeartBirth) {
+        this.yeartBirth = yeartBirth;
+    }
+
+    public int getYeartBirth() {
+        return yeartBirth;
+    }
+
+    public int getYearNow() {
+        return yearNow;
+    }
+
+    public void setYeartBirth(int yeartBirth) {
+        this.yeartBirth = yeartBirth;
+    }
+    public int hitungUmur(){
+        return 0;
+    }
+    public String tandanyaKamu(int umur){
+        return "";
+    }
+    
+}
---
+++ src/uts/if10/pkg10119913/muhammadalvinrizqiramadhan/no1/UTSIF1010119913MuhammadAlvinRizqiRamadhanNo1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if10.pkg10119913.muhammadalvinrizqiramadhan.no1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class UTSIF1010119913MuhammadAlvinRizqiRamadhanNo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
